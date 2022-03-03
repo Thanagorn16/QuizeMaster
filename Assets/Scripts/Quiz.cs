47098e863400696e93766bd51616c6c179671129
Thanagorn16 <thanagorn1611@gmail.com>
2022-03-03 15:11:57 +0700

    Try to connect the timer (hafly done). May do it again in the future.

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -6,18 +6,42 @@ using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
+    [Header("Questions")]
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+
+
+    [Header("Answers")]
     [SerializeField] GameObject[] answerButtons;
     int correctAnswerIndex;
+    bool hasAnsweredEarly;
+
+
+    [Header("Button Colors")]
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
+
+
+    [Header("Timer")]
+    [SerializeField] Image timerImage;
+    Timer timer;
 
 
     void Start()
     {
         // DisplayQuestion();
+        timer = FindObjectOfType<Timer>();
         GetNextQuestion();
+    }
+
+    void Update()
+    {
+        timerImage.fillAmount = timer.fillFraction;
+        if(timer.loadNextQuestion)
+        {
+            GetNextQuestion();
+            timer.loadNextQuestion = false;
+        }
     }
 
     void GetNextQuestion()
@@ -67,6 +91,7 @@ public class Quiz : MonoBehaviour
         }
 
         SetButtonState(false);
+        timer.CancelTimer();
     }
 
     void SetDefualButtonSprite()
